37d18e0ddceba37317b961bfb5329e330082be03
hek <whitekanon88@hotmail.com>
2024-11-19 16:07:11 +0800

    update order manager

---
+++ include/order_manager.h
@@ -0,0 +1,23 @@
+//
+// Created by Hongchuan JIANG on 19/11/24.
+//
+
+#ifndef ENGINE_TAKEHOME_ORDER_MANAGER_H
+#define ENGINE_TAKEHOME_ORDER_MANAGER_H
+
+#include <map>
+#include "order_book.h"
+
+class OrderManager {
+public:
+    OrderManager() {}
+
+    void readFromInputFile (const std::string& filePath);
+
+private:
+    std::map<std::string, OrderBook> _orderBooks;
+};
+
+
+
+#endif //ENGINE_TAKEHOME_ORDER_MANAGER_H
---
+++ src/order_manager.cpp
@@ -0,0 +1,46 @@
+//
+// Created by JIANG on 19/11/24.
+//
+
+
+#include "./../include/order_manager.h"
+#include <fstream>
+
+void OrderManager::readFromInputFile (const std::string& filePath) {
+    std::ifstream inputFile(filePath);
+    if (!inputFile) {
+        std::cerr << "Error: cannot open the file: " << filePath << std::endl;
+        return;
+    }
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        // init order from line
+        Order order(line);
+        // check if order book has been created for this symbol
+        if (_orderBooks.find(order.instrument) == _orderBooks.end()) {
+            // create new order book with that symbol
+            _orderBooks.insert(std::make_pair(order.instrument, OrderBook(order.instrument)));
+        }
+        // insert order to the order book
+        _orderBooks.at(order.instrument).addOrder(order);
+        // do matching
+        _orderBooks.at(order.instrument).doMatching();
+    }
+    // close file
+    inputFile.close();
+
+    // print trades
+    for (const auto& orderBook : _orderBooks) {
+        orderBook.second.printTrades();
+    }
+
+    // print new line as required
+    std::cout << std::endl;
+
+    // print order book
+    for (const auto& orderBook : _orderBooks) {
+        orderBook.second.printOrderBook();
+    }
+
+
+}
