03c93c3f34a9ca246ae640288f8362168979adde
hek <whitekanon88@hotmail.com>
2024-11-20 12:06:36 +0800

    update main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include <boost/program_options.hpp>
+#include "include/order_manager.h"
 
 namespace po = boost::program_options;
 
-int main() {
-  std::cerr << "====== Match Engine =====" << std::endl;
-  std::cerr << "Enter 'exit' to quit" << std::endl;
-  std::string line;
-  while (getline(std::cin, line) && line != "exit") {
-    std::cout << "Received: '" << line << "'" << std::endl;
-  }
-  return 0;
+void startMainProgram() {
+    // create order book manager
+    OrderManager orderManager;
+    // read from input file
+    orderManager.readFromInputFile("./tests/sample_input.txt");
+
+}
+
+int main(int argc, char* argv[]) {
+    // Define the options
+    po::options_description desc("Allowed options");
+    desc.add_options()
+            ("help,h", "Show help message")
+            ("start, s", "Start the program");
+
+    // Variable map to store the parsed options
+    po::variables_map vm;
+
+    try {
+        // Parse the command-line arguments
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm); // Notify to enforce required options and defaults
+
+        // Display help if the "help" option is present
+        if (vm.count("help")) {
+            std::cout << desc << "\n";
+            return 0;
+        }
+
+        // Access the "compression" option
+        if (vm.count("start")) {
+            std::cout << "====== Match Engine =====\n\n";
+            startMainProgram();
+        }
+
+    } catch (const po::error& e) {
+        // Handle errors in parsing
+        std::cerr << "Error: " << e.what() << "\n";
+        std::cerr << desc << "\n";
+        return 1;
+    }
 }
