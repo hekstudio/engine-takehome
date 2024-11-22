d1a4a22c61d6bc594466c2f0cab9c848e8ebd680
hek <whitekanon88@hotmail.com>
2024-11-22 12:18:27 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the Docker image
-docker build -t engine-takehome:latest -f ./deploy/Dockerfile . --progress=plain
+docker build -t engine-takehome:latest -f ./deploy/production/Dockerfile . --progress=plain
 
 # Run the Docker container
 docker run -i engine-takehome:latest
